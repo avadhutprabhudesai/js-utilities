5a276c26c64146b9bd7f254040f47836e67b37c4
avadhut <avadhutprabhudesai23@gmail.com>
2021-10-25 15:35:17 +0530

    lodash/array wip.

--- src/array/index.js
+++ src/array/index.js
@@ -1,3 +1,65 @@
 export const chunk = (arr, size = 1) => {
   if (size === 0) return [];
+  return arr.reduce((res, val, idx, array) => {
+    return idx === 0 || idx % size === 0
+      ? [...res, array.slice(idx, idx + size)]
+      : res;
+  }, []);
 };
+
+export const compact = (arr) => arr.filter((a) => a);
+
+export const concat = (arr, ...values) => {
+  return [
+    ...arr,
+    ...values.reduce(
+      (acc, val) => (Array.isArray(val) ? [...acc, ...val] : [...acc, val]),
+      []
+    ),
+  ];
+};
+
+export const difference = (arr, values) =>
+  arr.reduce((acc, val) => (!values.includes(val) ? [...acc, val] : acc), []);
+
+export const differenceBy = (arr, values, iteratee) => {
+  if (!(iteratee && typeof iteratee === 'function')) {
+    return difference(arr, values);
+  }
+  return arr.reduce(
+    (acc, val) =>
+      !values.map(iteratee).includes(iteratee(val)) ? [...acc, val] : acc,
+    []
+  );
+};
+
+export const differenceWith = (arr, values, comparator) => {
+  if (!(comparator && typeof comparator === 'function')) {
+    return difference(arr, values);
+  } else {
+    return arr.reduce(
+      (acc, val) =>
+        !values.some((v) => comparator(val, v)) ? [...acc, val] : acc,
+      []
+    );
+  }
+};
+
+export const drop = (arr, n = 1) => arr.slice(0 + n);
+
+export const dropRight = (arr, n = 1) => arr.slice(0, arr.length - n);
+
+export const dropWhile = (arr, pred = (x) => x) =>
+  arr.reduce((acc, val) => {
+    if (!acc.length) {
+      if (!pred(val)) {
+        return [...acc, val];
+      }
+      return acc;
+    } else {
+      return [...acc, val];
+    }
+  }, []);
+
+export const dropRightWhile = (arr, pred = (x) => x) =>
+  dropWhile(arr.reverse(), pred).reverse();
--- src/array/index.test.js
+++ src/array/index.test.js
@@ -1,11 +1,202 @@
+import {
+  chunk,
+  compact,
+  concat,
+  difference,
+  differenceBy,
+  differenceWith,
+  drop,
+  dropRight,
+  dropRightWhile,
+  dropWhile,
+} from '.';
+import { negate } from '../lodash/function';
+
+const isEven = (x) => x % 2 === 0;
+const isOdd = negate(isEven);
+const gte = (l, r) => l >= r;
+
 describe('Testing lodash/chunk', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(chunk([1, 2, 3, 4], 1))).toBeTruthy();
+  });
   it('should return an empty array when size is 0 ', () => {
     expect(chunk([1, 2, 3, 4], 0)).toEqual([]);
   });
   it('should return an array containing chunk-sized arrays', () => {
     expect(chunk([1, 2], 1)).toEqual([[1], [2]]);
+    expect(chunk([1, 2, 3, 4], 2)).toEqual([
+      [1, 2],
+      [3, 4],
+    ]);
+    expect(chunk([1, 2, 3, 4, 5], 3)).toEqual([
+      [1, 2, 3],
+      [4, 5],
+    ]);
   });
   it('should have a last chunk with remaining elements if input array cannot be split evenly ', () => {
     expect(chunk([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [4]]);
+  });
+});
+
+describe('Testing lodash/compact', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(compact([1, 2, 3, 4]))).toBeTruthy();
+  });
+  it('should return a blank array if input is blank array', () => {
+    expect(compact([])).toEqual([]);
+  });
+  it('should remove all falsy values', () => {
+    expect(compact([1, 0, false, 2, '', NaN, 3, undefined, 4, null])).toEqual([
+      1, 2, 3, 4,
+    ]);
+  });
+});
+
+describe('Testing lodash/concat', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(concat([], 1, 2))).toBeTruthy();
+  });
+  it('should concat given values to the input array', () => {
+    expect(concat([1, 2], 3, 4)).toEqual([1, 2, 3, 4]);
+  });
+  it('should flatten and concat arrays, if any, in the given values to the input array', () => {
+    expect(concat([1, 2], 3, [4], [[5]])).toEqual([1, 2, 3, 4, [5]]);
+  });
+});
+
+describe('Testing lodash/difference', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(difference([1, 2], [2, 4]))).toBeTruthy();
+  });
+  it('should return an array of all values from input array which are not present in values', () => {
+    expect(difference([1, 2], [2, 4])).toEqual([1]);
+    expect(difference([1, 2, undefined, null, NaN], [2, 4, null, NaN])).toEqual(
+      [1, undefined]
+    );
+    expect(difference([1, 2, true, false], [2, 4, null, false, true])).toEqual([
+      1,
+    ]);
+  });
+  it('should return a blank array if all values from input array are present in values', () => {
+    expect(difference([1, 2], [2, 1])).toEqual([]);
+  });
+});
+
+describe('Testing lodash/differenceBy', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(differenceBy([1, 2], [2, 4]))).toBeTruthy();
+  });
+  it('should work like difference when iteratee is not provided', () => {
+    expect(differenceBy([1, 2], [2, 4])).toEqual([1]);
+    expect(
+      differenceBy([1, 2, undefined, null, NaN], [2, 4, null, NaN])
+    ).toEqual([1, undefined]);
+    expect(
+      differenceBy([1, 2, true, false], [2, 4, null, false, true])
+    ).toEqual([1]);
+  });
+  it('should return an array of elements from input array not included in values by invoking iteratee on both of them', () => {
+    expect(differenceBy([1.1, 2.4], [2.5, 4.2], Math.floor)).toEqual([1.1]);
+    expect(differenceBy([1, 3, 5], [2, 4], isOdd)).toEqual([1, 3, 5]);
+    expect(differenceBy([2, 4, 6], [22, 44], isEven)).toEqual([]);
+  });
+});
+
+describe('Testing lodash/differenceWith', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(differenceWith([1, 3, 4], [2, 3], gte))).toBeTruthy();
+  });
+  it('should behave as difference if the comparator function is not passed in', () => {
+    expect(differenceWith([1, 2, 3], [2, 3])).toEqual([1]);
+  });
+  it('should return an array with elements from input array which are not included in the values array. The comparison should happen with the comparator function', () => {
+    expect(differenceWith([1, 2, 3], [6, 9], gte)).toEqual([1, 2, 3]);
+    expect(differenceWith([1, 2, 3], [0, 2], gte)).toEqual([]);
+  });
+});
+
+describe('Testing lodash/drop', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(drop([1, 3, 4], 1))).toBeTruthy();
+  });
+  it('should return a blank array if n is greater than or equal to the number of elements of the input array', () => {
+    expect(drop([1, 3], 4)).toEqual([]);
+    expect(drop([1, 2, 3, 4], 4)).toEqual([]);
+  });
+  it('should return the input array as is if n is 0', () => {
+    expect(drop([1, 2, 4], 0)).toEqual([1, 2, 4]);
+  });
+  it('should default to 1 if n is not provided', () => {
+    expect(drop([1, 3])).toEqual([3]);
+  });
+  it('should return a slice of an array with n elements dropped from the beginning', () => {
+    expect(drop([1, 2, 3, 4], 2)).toEqual([3, 4]);
+  });
+});
+
+describe('Testing lodash/dropRight', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(dropRight([1, 3, 4], 1))).toBeTruthy();
+  });
+  it('should return a blank array if n is greater than or equal to the number of elements of the input array', () => {
+    expect(dropRight([1, 3], 4)).toEqual([]);
+    expect(dropRight([1, 2, 3, 4], 4)).toEqual([]);
+  });
+  it('should return the input array as is if n is 0', () => {
+    expect(dropRight([1, 2, 4], 0)).toEqual([1, 2, 4]);
+  });
+  it('should default to 1 if n is not provided', () => {
+    expect(dropRight([1, 3])).toEqual([1]);
+  });
+  it('should return a slice of an array with n elements dropped from the end', () => {
+    expect(dropRight([1, 2, 3, 4], 2)).toEqual([1, 2]);
+  });
+});
+
+describe('Testing lodash/dropWhile', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(dropWhile([1, 2, 3], (x) => x))).toBeTruthy();
+  });
+  it('should return blank array when predicate is not provided', () => {
+    expect(dropWhile([1, 3, 4])).toEqual([]);
+  });
+  it('should return elements of input array exluding the dropped elements. Elements are dropped from the beginning for which the predicate is falsey', () => {
+    var users = [
+      { user: 'barney', active: false },
+      { user: 'fred', active: false },
+      { user: 'pebbles', active: true },
+    ];
+    expect(dropWhile([1, 3, 4], isEven)).toEqual([1, 3, 4]);
+    expect(
+      dropWhile(users, function (o) {
+        return !o.active;
+      })
+    ).toEqual([{ user: 'pebbles', active: true }]);
+  });
+});
+
+describe('Testing lodash/dropWhileRight', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(dropRightWhile([1, 2, 3], (x) => x))).toBeTruthy();
+  });
+  it('should return a blank array when no predicte is provided.', () => {
+    expect(dropWhile([1, 3, 4])).toEqual([]);
+  });
+  it('should return elements of input array exluding the dropped elements. Elements are dropped from the end for which the predicate is falsey', () => {
+    var users = [
+      { user: 'barney', active: true },
+      { user: 'fred', active: true },
+      { user: 'pebbles', active: false },
+    ];
+    expect(dropRightWhile([1, 3, 4], isEven)).toEqual([1, 3]);
+    expect(
+      dropRightWhile(users, function (o) {
+        return !o.active;
+      })
+    ).toEqual([
+      { user: 'barney', active: true },
+      { user: 'fred', active: true },
+    ]);
   });
 });
